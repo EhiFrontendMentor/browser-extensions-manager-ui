833f84d891de042d13ae0e13680f322011a1933e
Ehi Ejakhian <ehiejakhian@gmail.com>
2026-06-25 19:21:09 +0100

    finally published. Fix: Image paths

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,25 @@ import Header from "./components/Header.jsx";
 import Grid from "./components/Grid.jsx";
 import { ExtensionsProvider } from "./context/ExtensionsContext.jsx";
 import "./scss/components/Attribution.scss";
+import { Helmet } from "react-helmet";
 
 function App() {
-  return <ExtensionsProvider>
-    <Header></Header>
-    <Grid></Grid>
-    <div className="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href="https://ehiejakhian.github.io/">Ehi Ejakhian</a>.
-    </div>
-  </ExtensionsProvider>
+  return (
+    <>
+      <Helmet>
+        <title>Ehi Ejakhian | Browser Extensions Manager</title>
+        <link rel="icon" href="src/assets/images/favicon-32x32.png" sizes="32x32" type="image/png"/>
+      </Helmet>
+      <ExtensionsProvider>
+        <Header></Header>
+        <Grid></Grid>
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="https://ehiejakhian.github.io/">Ehi Ejakhian</a>.
+        </div>
+      </ExtensionsProvider>
+    </>
+  )
 }
 
 
