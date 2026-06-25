38d2afdbf82c709c86175daf6045c5ba879ff871
Ehi Ejakhian <ehiejakhian@gmail.com>
2026-06-25 19:39:12 +0100

    Fix: meta info

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,10 @@ import Header from "./components/Header.jsx";
 import Grid from "./components/Grid.jsx";
 import { ExtensionsProvider } from "./context/ExtensionsContext.jsx";
 import "./scss/components/Attribution.scss";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
-      <Helmet>
-        <title>Ehi Ejakhian | Browser Extensions Manager</title>
-        <link rel="icon" href="src/assets/images/favicon-32x32.png" sizes="32x32" type="image/png"/>
-      </Helmet>
       <ExtensionsProvider>
         <Header></Header>
         <Grid></Grid>
